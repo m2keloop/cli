fa8324148e5efb3fbdc159ecf1d0dd8c1c37a638
m2keloop <m2keloop@163.com>
2021-11-22 11:17:54 +0800

    t2d

---
+++ cmd/db/t2d.go
@@ -0,0 +1,67 @@
+package db
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+	"text/template"
+)
+
+const (
+	INSERT_TEMPLATE = `func (c *{{.model}}Dao) Get{{.model}}ByItem(ctx context.Context, model *model.{{.model}}) (has bool, err error) {
+	has, err = getMassDB().Context(ctx).Get(model)
+	if err != nil {
+		return
+	}
+	return
+}
+
+`
+	UPDATE_TEMPLATE = `func (c *{{.model}}Dao) Update{{.model}}ByItem(ctx context.Context, model *model.{{.model}}) (err error) {
+	if model.Id <= 0 {
+		err = errors.New("Update id can't not nil")
+		return
+	}
+	_, err = getMassDB().Context(ctx).ID(model.Id).Update(model)
+	return
+}
+
+`
+	GET_TEMPLATE = `func (c *{{.model}}Dao) Get{{.model}}ByItem(ctx context.Context, model *model.{{.model}}) (has bool, err error) {
+	has, err = getMassDB().Context(ctx).OrderBy("id desc").Get(model)
+	if err != nil {
+		return
+	}
+	return
+}
+
+`
+	ALL = INSERT_TEMPLATE + UPDATE_TEMPLATE + GET_TEMPLATE
+)
+
+var tableName string
+
+var t2dCmd = &cobra.Command{
+	Use:   "t2d",
+	Short: "表结构dao生成",
+	Long:  "表结构dao生成",
+	Run: func(cmd *cobra.Command, args []string) {
+		tmpl, err := template.New("t2d").Parse(ALL)
+		if err != nil {
+			return
+		}
+		data := map[string]string{
+			"model": tableName,
+		}
+		err = tmpl.Execute(os.Stdout, data)
+		if err != nil {
+			return
+		}
+	},
+}
+
+func GetCmd() *cobra.Command {
+	return t2dCmd
+}
+func init() {
+	t2dCmd.Flags().StringVarP(&tableName, "table", "t", "", "请输入表明")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,15 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"cli/cmd/db"
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{}
 
 func init() {
 	rootCmd.AddCommand(wordCmd)
+	rootCmd.AddCommand(db.GetCmd())
 }
 
 func Executor() error {
